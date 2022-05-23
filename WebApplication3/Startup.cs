a27597f18c0ebe1f3879bd6df3f3e28de0565a0c
ansheng3232 <87800345+ansheng3232@users.noreply.github.com>
2022-05-23 16:18:45 +0800

    Update Startup.cs

--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -18,7 +18,7 @@ namespace WebApplication3
         {
             Configuration = configuration;
         }
-
+          //fetch
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
